bf3f0c1e7d86e606ed2614acbc6fc661e3ba729c
mjkoster <michaeljohnkoster@gmail.com>
2015-12-30 21:08:09 -0800

    Resource model
    first code for Resource Model base class

--- model/ResourceModel.py
+++ model/ResourceModel.py
@@ -9,8 +9,11 @@
     separate the model handling from the serialization and for example 
     an alternate collection+senml+xml format could be easily supported.
 """
+from SenmlCollectionHandler import SenmlCollection
 from Links import Links
 from Items import SenmlItems
+import json
+import terms as v
 
 class ResourceModel():
     def __init__(self):
@@ -18,10 +21,16 @@ class ResourceModel():
         self._resourceNodeArray = []
         
     def serialize(self):
-        pass
+        self._result = []
+        for node in self._resourceNodeArray :
+            self._result.append(node.getModel())
+        return json.dumps(self._result)
     
-    def load(self):
-        pass
+    def load(self, jsonString):
+        self._loadObject = json.loads(jsonString)
+        if isinstance(list, self._loadObject):
+            for nodeMap in self._loadObject:
+                self.addNode(ResourceNode(nodeMap))
     
     def createOnServer(self):
         pass
@@ -30,14 +39,38 @@ class ResourceModel():
         pass
     
     def addNode(self, node):
-        """ find the correct place in the node array to insert the node """
-        pass
+        self._resourceNodeArray.append(node)
     
     def removeNode(self, path, selectMap):
         """ remove the selected node and all subresource nodes """
         pass
     
 class ResourceNode():
-    def __init__(self):
-        self._items = SenmlItems()
-        self._links = Links()
+    def __init__(self, nodeMap):
+        
+        self._links = Links(nodeMap[v._l])
+        self._items = SenmlItems(nodeMap[v._e])
+        self._baseName = nodeMap[v._bn]
+        self._resource = SenmlCollection(self._links, self._items, self._baseName)
+
+    def serialize(self):
+        return self._resource.serialize()
+    
+    def load(self, jsonString):
+        self._resource.load(jsonString)
+        
+    def getModel(self):
+        return self._resource._senml
+    
+    
+def selfTest():
+    model = ResourceModel
+    model.load("""[ {"bn": "/", "e": [], "l": [{"href": "", "rel": ["self"]}, {"href": "test", "rel": "sub"}]} ]""")
+    print model.serialize()
+    
+if __name__ == "__main__" :
+    selfTest()
+    
+    
+    
+    
