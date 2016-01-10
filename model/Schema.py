86573a60849a5f8e9a4f58af170fdc5c8e722dc0
mjkoster <michaeljohnkoster@gmail.com>
2016-01-09 18:45:42 -0800

    Schema refactor
    schema concepts refined and refactored

--- model/Schema.py
+++ model/Schema.py
@@ -5,18 +5,6 @@ schema for abstract resource models
 import terms as v
 import json
 
-BaseContext = { 
-    v._context: "http://schema.org",
-    v._schema: [
-        { v._property: v._uses,
-        v._description: """to point to a class that can be used by this class""",
-        },
-        { v._property: v._usedBy,
-        v._description: """to point to a class that uses this class""",
-        },
-    ]
-}
-
 
 class Schema:
     def __init__(self, nodeArray=None):
@@ -41,9 +29,9 @@ class SchemaNode:
         
     def configure(self, nodeMap):
         self._nodeMap = nodeMap
-        self._class = nodeMap[v._class]
+        self._rdfClass = nodeMap[v._rdfClass]
         self._subClassOf = nodeMap[v._subClassOf] 
-        self._uses = nodeMap[v._uses]
+        self._mayHave = nodeMap[v._mayHave]
         self._usedBy = nodeMap[v._usedBy]
         self._description = nodeMap[v._description]
         return self
--- model/W3Cschema.py
+++ model/W3Cschema.py
@@ -1,70 +1,87 @@
 import terms as v
 import json
 
+
+BaseContext = { 
+    v._schema: [
+        { v._rdfProperty: v._mayHave,
+         v._range : v._rdfClass,
+         v._domain: v._rdfClass,
+        v._description: """ classes that this class may have instances of or links to""",
+        },
+        { v._rdfProperty: v._usedBy,
+         v._range : v._rdfClass,
+         v._domain: v._rdfClass,
+        v._description: """ a class that may have or links to an instance of this class """,
+        },
+    ]
+}
+
+
 W3Cschema = { 
     v._context: "http://schema.org",
     v._schema: [
-        {v._class: "WoTinteractionModel",
+        {v._rdfClass: "WoTinteractionModel",
         v._subClassOf: "InteractionModel",
         v._usedBy: ["W3C_WoT_IG"],
-        v._uses: [v._thing, v._event, v._action, v._property, 
+        v._mayHave: [v._thing, v._event, v._action, v._property, 
                     v._capability, v._index, 
                     v._actuation, v._subscription, v._notification, 
                     v._value, v._params],
         v._description: """Base class for W3C Thing Model Interactions"""
         },
-        {v._class: v._index,
+        {v._rdfClass: v._index,
         v._subClassOf: "WoTinteractionModel",
         v._usedBy: [v._index, v._thing],
-        v._uses: [v._thing, v._capability, v._index],
+        v._mayHave: [v._thing, v._capability, v._index],
         v._description: """Index class"""
         },
-        {v._class: v._capability,
+        {v._rdfClass: v._capability,
         v._subClassOf: v._index,
         v._usedBy: [v._index, v._thing],
-        v._uses: [v._event, v._action, v._property, v._capability],
+        v._mayHave: [v._event, v._action, v._property, v._capability],
         v._description: """Capability class"""
         },
-        {v._class: v._thing,
+        {v._rdfClass: v._thing,
         v._subClassOf: v._capability,
         v._usedBy: [v._index],
-        v._uses: [v._event, v._action, v._property, v._capability, v._index],
+        v._mayHave: [v._event, v._action, v._property, v._capability, v._index],
         v._description: """Thing class"""
         },
-        {v._class: v._event,
+        {v._rdfClass: v._event,
         v._subClassOf: "WoTinteractionModel",
         v._usedBy: [v._thing, v._capability],
-        v._uses: [v._subscription, v._notification, v._params],
+        v._mayHave: [v._subscription, v._notification, v._params],
         v._description: """Event class"""
         },
-        {v._class: v._action,
+        {v._rdfClass: v._action,
         v._subClassOf: "WoTinteractionModel",
         v._usedBy: [v._thing, v._capability],
-        v._uses: [v._actuation, v._notification, v._params],
+        v._mayHave: [v._actuation, v._notification, v._params],
         v._description: """Action class"""
         },
-        {v._class: v._property,
+        {v._rdfClass: v._property,
         v._subClassOf: "WoTinteractionModel",
         v._usedBy: [v._thing, v._capability],
-        v._uses: [v._value],
+        v._mayHave: [v._value],
         v._description: """Property class"""
         },
-        {v._class: v._subscription,
+        {v._rdfClass: v._subscription,
         v._subClassOf: "WoTinteractionModel",
         v._usedBy: [v._event],
-        v._uses: [v._notification, v._params],
+        v._mayHave: [v._notification, v._params],
         v._description: """Subscription class"""
         },
-        {v._class: v._actuation,
+        {v._rdfClass: v._actuation,
         v._subClassOf: "WoTinteractionModel",
         v._usedBy: [v._action],
-        v._uses: [v._notification, v._params],
+        v._mayHave: [v._notification, v._params],
         v._description: """Actuation class"""
         },
-        {v._class: v._notification,
+        {v._rdfClass: v._notification,
         v._subClassOf: "WoTinteractionModel",
         v._usedBy: [v._subscription, v._actuation, v._params],
-        v._uses: [],
+        v._mayHave: [],
         v._description: """Notification class"""
         }
     ]
