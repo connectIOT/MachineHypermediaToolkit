9fd28bea560738a06f19964ab66e7c89d93ccf10
mjkoster <michaeljohnkoster@gmail.com>
2016-01-17 18:27:54 -0800

    Big refactor
    Make only resource links now, add self links later

--- model/ResourceConstructor.py
+++ model/ResourceConstructor.py
@@ -39,6 +39,7 @@ _stop = "stop"
 """ this table is a stub for using the appropriate application schema 
     to determine the WoT resource type of a particular domain type """
 _domainType = {
+           _index: _index,
            _light: _thing,
            _onoff: _capability,
            _brightness: _capability,
@@ -57,14 +58,16 @@ _collections = [
                 "capabilities",
                 ]
 
+_rootNode = {v._name: v._null,
+             v._type: _index}
 
 class ResourceModelConstructor:
     def __init__(self, model=None):
         self._model = model
         self._resourceModel = ResourceModel()
+        self._root = _rootNode
         self._processModelNode(model[v._resource], "/")
         
-    
     def _processModelNode(self, node, basePath):
         self._node = node
         currentBasePath = basePath
@@ -72,11 +75,14 @@ class ResourceModelConstructor:
             self._name = self._resource[v._name]
             self._type = self._resource[v._type]
             if _domainType[self._resource[v._type]] in _WoTClass:
+                print currentBasePath, self._resource[v._name], self._resource[v._type], _domainType[self._resource[v._type]]
                 self._class = _WoTClass[_domainType[self._resource[v._type]]]
-                self._resourceModel.addNodes( self._class(currentBasePath, self._resource).getNode() )
+                self._newNode = self._class(currentBasePath, self._resource).getNode()
+                print self._newNode.serialize()
+                self._resourceModel.addNodes( self._newNode )
             for self._property in self._resource:
                 if self._property in _collections:
-                    self._processModelNode(self._resource[self._property], currentBasePath+self._name+"/")
+                    self._processModelNode(self._resource[self._property], currentBasePath+self._name+ "/")
 
     def serialize(self):
         return self._resourceModel.serialize()
@@ -106,112 +112,88 @@ class ResourceType:
         return self._node
     
 class Index(ResourceType):
+    
     _resourceTemplate = {
-        v._l: [
-            {v._href: v._null, v._rel: v._self, v._rt: _index}
-        ],
+        v._l:[ {
+            v._href: v._null, 
+            v._rel: [v._sub],
+            v._rt: [_index]
+        } ],
         v._e: []
     }
     
     def configure(self):
         self._node.load(json.dumps(self._resourceTemplate))
-        self.getNode()._resource._links.selectMerge( {v._rel: v._self}, {v._rt: self._type} )
-        for self._property in self._resource:
-            if self._property in _collections:
-                for self._item in self._resource[self._property]:
-                    print self._item
-                    self._itemMap = self._itemConstructor(_domainType[self._item[v._type]])
-                    self._itemMap[v._l][0][v._href] = self._item[v._name]
-                    self._itemMap[v._l][0][v._rt].append(self._item[v._type])
-                    self.getNode().load(json.dumps(self._itemMap))
-        return self._node
+        self.getNode()._resource._links.selectMerge( {}, {v._rt: self._type} )
+        """ remove and replace href """
+        self.getNode()._resource._links.selectMerge( {}, {v._href: None} )
+        self.getNode()._resource._links.selectMerge( {}, {v._href: self._name} )
+                            
+        return self.getNode
+
+
 
 class Capability(Index):
     
     _resourceTemplate = {
-        v._l: [
-            {v._href: v._null, v._rel: v._self, v._rt: _capability}
-        ],
+        v._l:[ {
+            v._href: v._null, 
+            v._rel: v._sub,
+            v._rt: _capability
+        } ],
         v._e: []
     }
     
-    def configure(self):
-        self._node.load(json.dumps(self._resourceTemplate))
-        self.getNode()._resource._links.selectMerge( {v._rel: v._self}, {v._rt: self._type} )
-        for self._property in self._resource:
-            if self._property in _collections:
-                for self._item in self._resource[self._property]:
-                    print self._item
-                    self._itemMap = self._itemConstructor(_domainType[self._item[v._type]])
-                    self._itemMap[v._l][0][v._href] = self._item[v._name]
-                    self._itemMap[v._l][0][v._rt].append(self._item[v._type])
-                    self.getNode().load(json.dumps(self._itemMap))
-        return self._node
-    
-    def _itemConstructor(self, item):
-        self._itemMap = {
-            _event: {
-                v._l:[ {
-                    v._href: v._null, 
-                    v._rel: [v._form, v._item],
-                    v._rt: [_event]
-                } ],
-                v._e: [ {
-                    v._n: v._null,
-                    v._fv: {}
-                }]
-            },
-                          
-            _action: {
-                v._l:[ {
-                    v._href: v._null, 
-                    v._rel: [v._form, v._item],
-                    v._rt: [_action]
-                } ],
-                v._e: [ {
-                    v._n: v._null,
-                    v._fv: {}
-                } ]
-            },
-                         
-            _property: {
-                v._l:[ {
-                    v._href: v._null, 
-                    v._rel: [v._sub],
-                    v._rt: [_property]
-                } ],
-                v._e: []
-            },
-                         
-            _capability: {
-                v._l:[ {
-                    v._href: v._null, 
-                    v._rel: [v._sub],
-                    v._rt: [_capability]
-                } ],
-                v._e: []
-            }
-        }
-        return self._itemMap[item]
-    
-    
 class Thing(Capability):
+    
     _resourceTemplate = {
-        v._l: [
-            {v._href: v._null, v._rel: v._self, v._rt: _thing}
-        ],
+        v._l: [ {
+            v._href: v._null, 
+            v._rel: v._sub, 
+            v._rt: _thing
+        } ],
         v._e: []
     }
     
 class Event(ResourceType):
-    pass
-
+    
+    _resourceTemplate =  {
+        v._l:[ {
+            v._href: v._null, 
+            v._rel: [v._form, v._item],
+            v._rt: [_event]
+        } ],
+        v._e: [ {
+            v._n: v._null,
+            v._fv: {}
+        }]
+    }
+    
 class Action(ResourceType):
-    pass
-
+    
+    _resourceTemplate =  {
+        v._l:[ {
+            v._href: v._null, 
+            v._rel: [v._form, v._item],
+            v._rt: [_action]
+        } ],
+        v._e: [ {
+            v._n: v._null,
+            v._fv: {}
+        } ]
+    }
+    
 class Property(ResourceType):
-    pass
-
+    
+    _resourceTemplate =  {
+        v._l:[ {
+            v._href: v._null, 
+            v._rel: [v._sub],
+            v._rt: [_property]
+        } ],
+        v._e: []
+    }
+    
 class Subscription(ResourceType):
     pass
 
