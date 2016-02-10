24429b052daa72f87bf129cc4c21917a1d2db8dc
mjkoster <michaeljohnkoster@gmail.com>
2016-02-09 16:31:31 -0800

    fix TOM index and test property get/set
    property get/set by TOM discovery label

--- MachineHypermediaToolkit/model/DiscoveryFilter.py
+++ MachineHypermediaToolkit/model/DiscoveryFilter.py
@@ -5,67 +5,67 @@ import json
 _filter = [
     {
         v._rt: d._light,
-        v._label: d._light,
+        v._label: "mylight",
         v._has: [
                  {
                   v._rt: d._brightness,
-                  v._label: d._brightness,
+                  v._label: "dimmer-control",
                   v._has:[
                     {
                         v._rt: d._currentbrightness,
-                        v._label: d._currentbrightness
+                        v._label: "current-level"
                     },
                     {
                         v._rt: d._targetbrightness,
-                        v._label: d._targetbrightness
+                        v._label: "target-level"
                     },
                     {
                         v._rt: d._transitiontime,
-                        v._label: d._transitiontime
+                        v._label: "brightness-ramp"
                     }
                 ]
             },
             {
                 v._rt: d._colorhs,
-                v._label: d._colorhs,
+                v._label: "color-control",
                 v._has:[
                     {
                         v._rt: d._currentcolorh,
-                        v._label: d._currentcolorh
+                        v._label: "current-hue"
                     },
                     {
                         v._rt: d._currentcolors,
-                        v._label: d._currentcolors
+                        v._label: "current-saturation"
                     },
                     {
                         v._rt: d._targetcolorh,
-                        v._label: d._targetcolorh
+                        v._label: "target-hue"
                     },
                     {
                         v._rt: d._targetcolors,
-                        v._label: d._targetcolors
+                        v._label: "target-saturation"
                     },
                     {
                         v._rt: d._transitiontime,
-                        v._label: d._transitiontime
+                        v._label: "color-ramp"
                     }
                 ]
              },
             {
                 v._rt: d._onoff,
-                v._label: d._onoff,
+                v._label: "onoff-control",
                 v._has:[
                     {
                         v._rt: d._currentstate,
-                        v._label: d._currentstate
+                        v._label: "current-onoff"
                     },
                     {
                         v._rt: d._targetstate,
-                        v._label: d._targetstate
+                        v._label: "target-onoff"
                     },
                     {
                         v._rt: d._delaytime,
-                        v._label: d._delaytime
+                        v._label: "onoff-delay"
                     }
                 ]
             }
